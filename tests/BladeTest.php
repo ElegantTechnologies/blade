d03e150e12149f348c8083d1ec35b1733e58d652
JJ Rohrer <jj.bitbucket@rohrer.org>
2023-08-22 12:59:26 -0400

    newer php compat

--- tests/BladeTest.php
+++ tests/BladeTest.php
@@ -14,7 +14,7 @@ class BladeTest extends TestCase
      */
     private $blade;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->blade = new Blade('tests/views', 'tests/cache');
 
